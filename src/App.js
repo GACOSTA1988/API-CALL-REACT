f4cf0ec1b7a31c6c14600ad38eed83894b5c6ff5
Student <example@example.com>
2020-03-02 16:39:29 -0800

    files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+      items: {},
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://api.musixmatch.com/ws/1.1/track.search?q_artist=enya&page_size=10&page=1&s_track_rating=desc&apikey=9be8fb4b24d5d7d4518870f05edaf848')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json.message.body.track_list,
+      })
+    });
+  }
+  render() {
+
+let { isLoaded, items } = this.state;
+
+if (!isLoaded) {
+  return <div> shit is loading! </div>
+}
+else {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {this.state.items.map( ({track}) => {
+      return track.track_name;
+    })}
+
     </div>
   );
+
 }
 
+  }
+}
 export default App;
+
+
+// {Object.keys(items).map(function(key) {
+//   let item = items.message.body.track_list[0][key];
+//   console.log(items.message.body.track_list[0].track.track_name);
+//   // return item.message.body.track_list[0].track.track_name;
+// })}
---
+++ src/Call.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Router, Route, Switch } from "react-router";
+
+
+export default class Call extends React.Component {
+state = {
+loading: true
+}
+
+componentDidMount(){
+
+}
+
+
+  render() {
+    return (
+    <div>
+
+    <p>sup</p>
+
+    </div>
+  )
+  }
+}
